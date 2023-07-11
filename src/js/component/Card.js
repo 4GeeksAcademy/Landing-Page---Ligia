632ae358b5610f520d99e2cce7d317bf121b0d4f
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-07-11 22:55:45 +0000

    commit

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -1,20 +1,30 @@
 import React from 'react'
+import Proptypes from 'prop-types'
 
-const Card = () => {
+let cardStyle = {
+  width: "18rem",
+}
+
+const Card = (props) => {
   return (
     <>
-
-<div ClassName="card" style="width: 18rem;">
-  <img src="..." ClassName="card-img-top" alt="..." />
-  <div ClassName="card-body">
-    <h5 ClassName="card-title">Card title</h5>
-    <p ClassName="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-    <a href="#" ClassName="btn btn-primary">Go somewhere</a>
-  </div>
-</div>
-
+      <div className='col-12 col-md-3'>
+        <div className="card">
+          <img src={props.image} className="card-img-top" alt="..." />
+          <div className="card-body">
+            <h5 className="card-title">Card title {props.mensaje} - {props.numero} </h5>
+            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" className="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
 
+Card.Proptypes = {
+  numero: Proptypes.number,
+  mensaje: Proptypes.string,
+  image: Proptypes.string
+}
 export default Card
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -5,12 +5,28 @@ import rigoImage from "../../img/rigo-baby.jpg";
 
 import Card from "./Card";
 
+let cardsInfo = [
+	{ numero: 85, mensaje: 'Hola a todas!',image:'https://picsum.photos/id/1/400/300' },
+	 { numero: 74, mensaje: 'Adios a todas!',image:'https://picsum.photos/id/103/400/300' },
+	  { numero: 15, mensaje: '4geeks academy',image:'https://picsum.photos/id/1048/400/300' },
+	   { numero: 15, mensaje: '4geeks academy',image:'https://picsum.photos/id/1025/400/300' }
+]
+
+
 //create your first component
 const Home = () => {
 	return (
-		<div>
-          <Card/>
+		<div className="container">
+			<div className="row">
+
+				{
+					cardsInfo.map((value, index) => {
+						return (<Card numero={value.numero} mensaje={value.mensaje} image={value.image} key={index} />)
+					})
+				}
+			</div>
 		</div>
+
 	);
 };
 
