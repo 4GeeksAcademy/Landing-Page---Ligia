9b0b6da3690e7d3a4e73d5da823ae061fd0d08ef
Ligia88 <129026864+Ligia88@users.noreply.github.com>
2023-07-12 00:41:05 +0000

    commit

--- src/js/component/Card.js
+++ src/js/component/Card.js
@@ -12,8 +12,8 @@ const Card = (props) => {
         <div className="card">
           <img src={props.image} className="card-img-top" alt="..." />
           <div className="card-body">
-            <h5 className="card-title">Card title {props.mensaje} - {props.numero} </h5>
-            <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <h5 className="card-title">The Sweet Charm of Innocence {props.mensaje} - {props.numero} </h5>
+            <p className="card-text">Jelly beans halvah halvah gingerbread oat cake. Danish soufflé gingerbread muffin sugar plum.</p>
             <a href="#" className="btn btn-primary">Go somewhere</a>
           </div>
         </div>
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Navbar from "./Navbar"
+
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
@@ -6,10 +8,10 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import Card from "./Card";
 
 let cardsInfo = [
-	{ numero: 85, mensaje: 'Hola a todas!',image:'https://picsum.photos/id/1/400/300' },
-	 { numero: 74, mensaje: 'Adios a todas!',image:'https://picsum.photos/id/103/400/300' },
-	  { numero: 15, mensaje: '4geeks academy',image:'https://picsum.photos/id/1048/400/300' },
-	   { numero: 15, mensaje: '4geeks academy',image:'https://picsum.photos/id/1025/400/300' }
+	{ numero: 85, mensaje: 'Card 1', image: 'https://picsum.photos/id/1/400/300' },
+	{ numero: 74, mensaje: 'Card 2', image: 'https://picsum.photos/id/103/400/300' },
+	{ numero: 15, mensaje: 'Card 3', image: 'https://picsum.photos/id/1048/400/300' },
+	{ numero: 15, mensaje: 'Card 4', image: 'https://picsum.photos/id/1025/400/300' }
 ]
 
 
